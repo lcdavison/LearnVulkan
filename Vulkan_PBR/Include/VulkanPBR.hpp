0c06044140db732f412982cc34b73933af1cb852
Luke Davison <luke.davison@talktalk.net>
2022-03-07 13:32:55 +0000

    Create VulkanPBR.hpp

---
+++ Vulkan_PBR/Include/VulkanPBR.hpp
@@ -0,0 +1,34 @@
+#pragma once
+
+#include <Windows.h>
+
+namespace PBR
+{
+	class VulkanApplication
+	{
+		VulkanApplication() = default;
+
+	public:
+		inline static VulkanApplication& Get()
+		{
+			static VulkanApplication Instance = VulkanApplication();
+			return Instance;
+		}
+
+		bool Initialise();
+		bool Run();
+
+	private:
+		static LRESULT CALLBACK WindowProcedure(HWND Window, UINT Message, WPARAM WParam, LPARAM LParam);
+
+	private:
+		inline static const TCHAR* kWindowClassName = TEXT("Vulkan-PBR");
+		inline static const TCHAR* kWindowName = TEXT("PBR Window");
+
+		inline static const uint32_t kDefaultWindowWidth = 1280u;
+		inline static const uint32_t kDefaultWindowHeight = 720u;
+
+		HWND WindowHandle_;
+
+	};
+}
