dfff30309b57efd2f3f496fb9341fca32ec7f6bd
Luke Davison <luke.davison@talktalk.net>
2022-04-15 15:49:26 +0100

    Vector implementation

--- Projects/Math/Include/Vector.hpp
+++ Projects/Math/Include/Vector.hpp
@@ -1,29 +1,57 @@
 #pragma once
 
-/* Check this can't remember intrinsic header */
-//#include <xmmintrin.h>
+#include <cmath>
 
 namespace Math
 {
-  struct Vector3
-  {
-      float X;
-      float Y;
-      float Z;
-  };
-  
-  extern Vector3 operator + (Vector3 const & Left, Vector3 const & Right);
-  
-  /* In an abstract sense we can also use this for difference between 3D Points */
-  extern Vector3 operator - (Vector3 const & Left, Vector3 const & Right);
-  
-  /* Dot product */
-  extern Vector3 operator * (Vector3 const & Left, Vector3 const & Right);
-  
-  /* Cross product */
-  extern Vector3 operator ^ (Vector3 const & Left, Vector3 const & Right);
-  
-  extern Vector3 operator * (Vector3 const & Vector, float const Scalar);
-  
-  extern Vector3 operator * (float const Scalar, Vector3 const & Vector);
+    /* This is currently just used as a container for SSE */
+    struct alignas(16) Vector4
+    {
+        float X;
+        float Y;
+        float Z;
+        float W;
+    };
+
+    struct Vector3
+    {
+        float X;
+        float Y;
+        float Z;
+
+        inline static float const Length(Vector3 const & Vector)
+        {
+            return std::sqrt(Vector.X * Vector.X + Vector.Y * Vector.Y + Vector.Z * Vector.Z);
+        }
+
+        inline static Vector3 const Normalize(Vector3 const & Vector)
+        {
+            return Vector * (1.0f / Length(Vector));
+        }
+
+        static constexpr Vector3 const Zero()
+        {
+            return Vector3 { 0.0f, 0.0f, 0.0f };
+        }
+
+        static constexpr Vector3 const One()
+        {
+            return Vector3 { 1.0f, 1.0f, 1.0f };
+        }
+    };
+
+    extern Vector3 operator + (Vector3 const & Left, Vector3 const & Right);
+
+    /* In an abstract sense we can also use this for difference between 3D Points */
+    extern Vector3 operator - (Vector3 const & Left, Vector3 const & Right);
+
+    /* Dot product */
+    extern float operator * (Vector3 const & Left, Vector3 const & Right);
+
+    /* Cross product */
+    extern Vector3 operator ^ (Vector3 const & Left, Vector3 const & Right);
+
+    extern Vector3 operator * (Vector3 const & Vector, float const Scalar);
+
+    extern Vector3 operator * (float const Scalar, Vector3 const & Vector);
 }
--- Projects/Math/Source/Vector.cpp
+++ Projects/Math/Source/Vector.cpp
@@ -1,1 +1,53 @@
 #include "Vector.hpp"
+
+Math::Vector3 Math::operator + (Math::Vector3 const & Left, Math::Vector3 const & Right)
+{
+    return Vector3
+    {
+        Left.X + Right.X,
+        Left.Y + Right.Y,
+        Left.Z + Right.Z,
+    };
+}
+
+Math::Vector3 Math::operator - (Math::Vector3 const & Left, Math::Vector3 const & Right)
+{
+    return Vector3
+    {
+        Left.X - Right.X,
+        Left.Y - Right.Y,
+        Left.Z - Right.Z,
+    };
+}
+
+float Math::operator * (Math::Vector3 const & Left, Math::Vector3 const & Right)
+{
+    return Left.X * Right.X +
+           Left.Y * Right.Y +
+           Left.Z * Right.Z;
+}
+
+Math::Vector3 Math::operator ^ (Math::Vector3 const & Left, Math::Vector3 const & Right)
+{
+    return Vector3
+    {
+        Left.Y * Right.Z - Left.Z * Right.Y,
+        Left.Z * Right.X - Left.X * Right.Z,
+        Left.X * Right.Y - Left.Y * Right.X,
+    };
+}
+
+Math::Vector3 Math::operator * (Math::Vector3 const & Vector, float const Scalar)
+{
+    return Vector3
+    {
+        Vector.X * Scalar,
+        Vector.Y * Scalar,
+        Vector.Z * Scalar,
+    };
+}
+
+Math::Vector3 Math::operator * (float const Scalar, Math::Vector3 const & Vector)
+{
+    return Vector * Scalar;
+}
