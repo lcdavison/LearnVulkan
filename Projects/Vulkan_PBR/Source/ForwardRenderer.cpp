5703e951f1639d2870fbb8a9f0e455794c3d3f2b
Luke Davison <luke.davison@talktalk.net>
2022-04-29 19:34:02 +0100

    Remove unnecessary create uniform buffer

--- Projects/Vulkan_PBR/Source/ForwardRenderer.cpp
+++ Projects/Vulkan_PBR/Source/ForwardRenderer.cpp
@@ -421,8 +421,6 @@ bool const ForwardRenderer::Initialise(VkApplicationInfo const & ApplicationInfo
 
         ::CreateFrameState();
 
-        ::CreatePerFrameUniformBuffer();
-
         /* Create Frame Allocation Block */
         Vulkan::Device::CreateBuffer(DeviceState, kFrameAllocationMemorySizeInBytes, VK_BUFFER_USAGE_UNIFORM_BUFFER_BIT, VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT | VK_MEMORY_PROPERTY_HOST_COHERENT_BIT, FrameAllocationBuffer, FrameAllocationMemory);
     }
