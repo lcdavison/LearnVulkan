e7dee702b79a9e394f13c21c60a7249c6d21fcd5
Luke Davison <luke.davison@talktalk.net>
2022-07-31 20:51:23 +0100

    Log assertion failures

--- Projects/Vulkan_PBR/Include/Common.hpp
+++ Projects/Vulkan_PBR/Include/Common.hpp
@@ -1,22 +1,26 @@
 #pragma once
 
 #include "CommonTypes.hpp"
+#include "Logging.hpp"
 
 #if _DEBUG
     #include <intrin.h>
 #endif
 
 #ifdef _UNICODE
-    #define PBR_TEXT(String) L##String
+    #define PBR_TEXT(Text) L##Text
 #else
-    #define PBR_TEXT(String) String
+    #define PBR_TEXT(Text) Text
 #endif
 
 #if _DEBUG
 #define PBR_ASSERT(Condition) \
-    if (!(Condition)) { \
-        __debugbreak(); \
-    }
+    do { \
+        if (!(Condition)) { \
+            Logging::Log(Logging::LogTypes::Error, String::Format(PBR_TEXT("Assertion Failed: %s"), #Condition)); \
+            __debugbreak(); \
+        } \
+    } while(false)
 #else
     /* TODO: Should probably throw an exception or something */
     #define PBR_ASSERT(Condition)
